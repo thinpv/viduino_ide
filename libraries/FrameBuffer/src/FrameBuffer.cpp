44762142d906c32e82863587e0c6c536005d508b
thinpv <thindcna@gmail.com>
2021-06-01 16:52:30 +0700

    add new lcd driver change lvgl to 16 bit

--- libraries/FrameBuffer/src/FrameBuffer.cpp
+++ libraries/FrameBuffer/src/FrameBuffer.cpp
@@ -1,5 +1,7 @@
 #include "FrameBuffer.h"
 
+extern void memcpy(void *, void *, int);
+
 FrameBuffer::FrameBuffer()
 {
 }
@@ -7,21 +9,37 @@ FrameBuffer::FrameBuffer()
 // ~FrameBuffer();
 int FrameBuffer::begin(int width, int height, int bright)
 {
-	fb_init(&framebuffer, width, height);
-	fb_setbl(&framebuffer, bright);
+	// fb_init(&framebuffer, width, height);
+	// fb_setbl(&framebuffer, bright);
+	BT1 = (unsigned short *)LCDbuff;
+	Sys_LCD_Init(XSIZE_PHYS, YSIZE_PHYS, (unsigned int *)LCDbuff, NULL);
+	this->width = width;
+	this->height = height;
 }
 
 void FrameBuffer::setBright(int bright)
 {
-	return fb_setbl(&framebuffer, bright);
+	// return fb_setbl(&framebuffer, bright);
 }
 
-void FrameBuffer::areaPresent(int x1, int x2, int y1, int y2, int *color_p)
+void FrameBuffer::areaPresent(int x1, int x2, int y1, int y2, unsigned short *data)
 {
-	return fb_area_present(&framebuffer, x1, x2, y1, y2, (uint32_t *)color_p);
+	uint16_t x = x2 - x1 + 1;
+	uint16_t size_of_row = x * sizeof(uint32_t);
+	for (int i = y1; i < y2 + 1; i++)
+	{
+		memcpy(&BT1[i * width + x1], data, size_of_row);
+		data += x;
+	}
+	// return fb_area_present(&framebuffer, x1, x2, y1, y2, (uint32_t *)color_p);
 }
 
-void FrameBuffer::pixelPresent(int x, int y, uint32_t data)
+// void FrameBuffer::pixelPresent(int x, int y, uint32_t data)
+// {
+// 	return fb_pixel_present(&framebuffer, x, y, data);
+// }
+
+unsigned short *FrameBuffer::getBuffer()
 {
-	return fb_pixel_present(&framebuffer, x, y, data);
+	return (unsigned short *)LCDbuff;
 }
--- libraries/FrameBuffer/src/FrameBuffer.h
+++ libraries/FrameBuffer/src/FrameBuffer.h
@@ -1,7 +1,8 @@
 #ifndef __FRAME_BUFFER_H_
 #define __FRAME_BUFFER_H_
 
-#include "fb.h"
+// #include "fb.h"
+#include <sys_lcd.h>
 
 #ifdef __cplusplus
 class FrameBuffer
@@ -11,11 +12,16 @@ public:
 	// ~FrameBuffer();
 	int begin(int width = 800, int height = 480, int bright = 100);
 	void setBright(int bright);
-	void areaPresent(int x1, int x2, int y1, int y2, int *color_p);
-	void pixelPresent(int x, int y, uint32_t data);
+	void areaPresent(int x1, int x2, int y1, int y2, unsigned short *color_p);
+	// void pixelPresent(int x, int y, uint32_t data);
+	unsigned short *getBuffer();
 
 private:
-	framebuffer_t framebuffer;
+	// framebuffer_t framebuffer;
+	int width;
+	int height;
+	unsigned short *BT1; //
+	__attribute__((aligned(1024))) unsigned int LCDbuff[XSIZE_PHYS * YSIZE_PHYS];
 };
 #endif
 
